2ca145c15811009a6f5c9212f8189ae79cd9af31
Mateus <mtsmlima@gmail.com>
2025-01-31 11:29:58 -0300

    Added guessesLeft dynamically

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
 import './headerStyle.scss'
+import PropTypes from 'prop-types'
 
 
-export default function Header() {
+Header.propTypes = {
+   guessesLeft: PropTypes.number.isRequired,
+}
+
+export default function Header(props) {
    return (
       <header className="header">
          <h1 className="header__title">Assembly: Endgame</h1>
 
          <p className="header__description">
-            Guess the word in under 8 attempts to keep the programming world safe from Assembly!
+            {`Guess the word in under ${props.guessesLeft} attempts to keep the programming world safe from Assembly!`}
          </p>
       </header>
    )
